0f52e78ac9f00faa76f5e360ad9ebc721817525a
CyGRada <carada@fullsail.edu>
2015-03-05 14:54:07 -0500

    Day2 Intro files staering
    Created html and js file

---
+++ Day 2/js/script.js
@@ -0,0 +1,17 @@
+/*
+Carlos Rada
+03/05/2015
+Section 00
+Day 2 Intro
+ */
+
+//alert("Testing to see if connected");
+
+/*
+Example of a multilined comment.
+Anything inside of here will not be seen by the computer.
+ */
+
+// Single line comment. ONLY this line will be ignored.
+
+// Alert box - pop up box that will alert the user with 
