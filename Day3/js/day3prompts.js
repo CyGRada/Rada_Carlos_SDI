793f6f595f11e23cf595028c7f8949b553f60f25
CyGRada <carada@fullsail.edu>
2015-03-07 15:27:46 -0500

    learn to prompt
    result

---
+++ Day3/js/day3prompts.js
@@ -0,0 +1,29 @@
+/*
+Carlos Rada
+Section 00
+3-7-15
+Day 3 prompt
+ */
+
+alert("Just Testing");
+
+//Ask the user for input
+// prompt("text that the user will")
+
+var userInput = prompt("Enter year of birth");
+console.log(userInput);
+
+//Calculate Area of Rectangle
+//Length * width
+
+//Prompt user for the width and height.
+//Explain to them whats going on.
+var height = prompt("Lets calculate the area of a rectangle! \n Please enter the height of the rectangle.");
+
+var width = prompt("Please enter the width of the rectangle.");
+
+var area = height * width;
+console.log("The are of the rectangle with a height of "+height+" and a width of "+width+" is "+area+".");
+
+var results = "The results of the area are "+area+"
+console.log(results);
