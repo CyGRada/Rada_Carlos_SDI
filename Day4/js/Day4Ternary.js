c769a9e3fc0820826329636657da29f799453398
CyGRada <carada@fullsail.edu>
2015-03-10 17:01:28 -0400

    Ternary
    How to spell

---
+++ Day4/js/Day4Ternary.js
@@ -0,0 +1,49 @@
+
+ /*
+Carlos Rada
+Section 00
+Date: 3-10-15
+Assignment Name: Ternary
+ */
+
+//alert("Just Testing");
+
+ //if your GPA is over 2.0 then you can graduate
+
+ //Create a variable for GPA
+ var gpa = 3.5;
+
+ //Basic Conditional
+ if(gpa >2.0){
+     console.log("You can graduate");
+ } else {
+     console.log("You cant graduate");
+ }
+
+ //(condition to test) ? code to run if true : code to run if false;
+
+ // Create a basic ternary function for our gpa
+ (gpa > 2.0) ? console.log ("You can graduate") : console.log("You cant graduate");
+
+ //If a child is under 10 then they must read Green eggs & Ham otherwise they can read the time machine
+
+ //Create a variable for child age and for the book
+ var age = 16;
+ var book;
+
+ // Use a ternary to define the book
+
+ book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+
+ console.log("The kid should read "+book+".");
+
+ // Value of book if basic conditional
+
+ var bookBasic;
+
+ if(age < 10){
+   bookBasic = "Green Eggs and Ham";
+ } else {
+     bookBasic = "The Time Machine";
+ }
+ console.log(bookBasic);
