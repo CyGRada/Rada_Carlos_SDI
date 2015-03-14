d92f472f6dad9b743d561c379b28d3e1af713a0f
CyGRada <carada@fullsail.edu>
2015-03-14 16:34:20 -0400

    Day6
    While and Do While Loops

--- Day6WhileLoops/js/Day6WhileLoops.js
+++ Day6WhileLoops/js/Day6WhileLoops.js
@@ -6,4 +6,40 @@ Date: 3-14-15
 Assignment Name: Day 6 While Loops
  */
 
-alert("Just Testing");
+//alert("Just Testing");
+
+ //Basic While Loop
+
+ //Start  a counter variable
+ var counter = 0;
+
+ //Start while loop
+ //Condition to test goes inside of ()
+ while(counter<200){
+     console.log("somethings never end");
+     console.log(counter);
+
+     //make a change to the counting variable
+     counter+=50;
+
+ }
+
+
+ //Do While Loop
+ //This Loop will run the code first then check a condition!!
+
+ /*
+   do{
+    //Code to run
+ } while(condition to test);
+  */
+
+ //Declare  a counting variable
+ var i = 20;
+
+ do{
+     console.log("The number is "+i);
+
+     //change the counting variable
+     i++;
+ } while(i < 10);
