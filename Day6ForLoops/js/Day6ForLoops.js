6de10713ae81c2228f65f2004a49760cddfe8474
CyGRada <carada@fullsail.edu>
2015-03-14 16:59:00 -0400

    Day 6 for Loops
    final

--- Day6ForLoops/js/Day6ForLoops.js
+++ Day6ForLoops/js/Day6ForLoops.js
@@ -6,4 +6,84 @@ Date:3-14-15
 Assignment Name: Day 6 for Loops
  */
 
-alert("Just Testing");
+//alert("Just Testing");
+
+
+ // for(initialization; condition; increment of change;) {}
+ // we use i ALOT in loops  -  because it stands for iteration - time around
+
+ for(var i = 0; i < 20; i++){
+     console.log("The value of i is "+i);
+ }
+
+
+
+ console.log(i);
+
+// Break in a loop
+
+ for(var j = 0; j < 5; j++){
+
+   //conditional to test if we should break the loop
+     //if the value j is 3, then console.log() something different and stop the the loop
+
+     if(j === 3){
+         console.log("J is 3!!");
+         break;
+     }
+
+     console.log("The value of j is "+j);
+
+
+
+ }
+
+
+
+ /// (var j = 5; j>0; j--)
+
+
+ // Specific cases for each value
+
+ for(var k = 0; k < 3; k++){
+
+     if(k === 0){
+         console.log("The value is zero");
+     } else if (k === 1){
+         console.log("The value is one");
+     } else if (k === 2){
+         console.log("The value is two");
+     }
+     
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
